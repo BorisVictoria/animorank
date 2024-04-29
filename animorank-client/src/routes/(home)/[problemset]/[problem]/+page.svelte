5e83723ae3bb0af5034e0dfa8bb175f718fb8462
Joel Ethan Batac <bjoelethan@gmail.com>
2024-04-29 23:36:25 +0800

    feat: Implement resizable panels on problem solving page.

--- animorank-client/src/routes/(home)/[problemset]/[problem]/+page.svelte
+++ animorank-client/src/routes/(home)/[problemset]/[problem]/+page.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
     import CodeEditor from '$lib/components/CodeEditor.svelte'
+    import { onMount } from 'svelte'
     export let data
 
     let problem = data.problem
+    let innerWidth : number = 0
+
+    let offset = 0
+
+    $: leftWidth = innerWidth / 2 + offset
+    $: rightWidth = innerWidth - leftWidth
+
+    $: console.log(leftWidth)
     
-    $: value = "#include<stdio.h>\n\nint main(){ \n\n  return 0;\n}"
+    let value = "#include<stdio.h>\n\nint main(){ \n\n  return 0;\n}"
 
     //TODO: Add proper type
     let setValue : () => void;
@@ -14,13 +23,32 @@
         const res = await fetch("/api", {method: "POST", body: JSON.stringify(value)})
     }
 
+    const startResize = (e : MouseEvent) => {
+        console.log('resizing')
+        let initial = e.clientX
+        console.log(initial)
+        document.onmousemove = (e) => {
+            offset -= leftWidth - e.clientX 
+        }
+    }
+
+    const abortResize = () => {
+        document.onmousemove = null
+    }
+
+    const resetResize = () => {
+
+        offset = 0
+    }
+
 </script>
 
+<svelte:window on:mouseup={abortResize} bind:innerWidth/>
 
 <!--Use grow class on your divs if you want no scroll bar, otherwise the content will just overflow all good-->
-<div class="flex justify-center grow overflow-auto">
+<div class="flex justify-start grow overflow-auto relative">
 
-    <div class="w-3/6 pl-5 pt-5 pr-3 pb-10 overflow-auto">
+    <div class=" pl-5 pt-5 pr-3 pb-10 overflow-auto" style={`width : ${leftWidth}px`}>
         <h1 class="text-2xl font-bold"> Two Sum </h1>
 
         <br/>
@@ -42,10 +70,15 @@
 
     </div>
 
-    <div class="w-1 bg-black/30"></div>
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <div class="w-2 cursor-col-resize flex justify-center items-center bg-black/30" id="divider" on:mousedown={startResize}> 
+        
+       <button class="w-full" title="reset sizing" on:click={resetResize}> | </button>
 
-    <div class="flex flex-col w-3/6">
-        <span class="flex items-end justify-between gap-3 px-3 w-full pt-1 pb-2">
+    </div>
+
+    <div class="flex flex-col" style={`width : ${rightWidth}px`}>
+        <span class="flex items-end justify-between gap-3 px-3 w-full pt-1 pb-2 grow">
             <div class="self-center justify-self-center"> code.c </div>
 
             <div class="flex gap-3">
