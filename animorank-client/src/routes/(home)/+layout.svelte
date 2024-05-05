1538c3c4683dc3050d008bbb1b07a2f6931ccd7e
christianvtia <christiantia04@gmail.com>
2024-05-05 16:25:30 +0800

    feat: Update navbar

--- animorank-client/src/routes/(home)/+layout.svelte
+++ animorank-client/src/routes/(home)/+layout.svelte
@@ -6,20 +6,46 @@
 <div class="flex flex-col h-screen">
 
     <!--Navbar start-->
-    <div class="navbar bg-base-100 h-16 border-b-2">
+    <div class="navbar bg-base-100 h-16 border-b-2 px-10">
+        
         <div class="flex-none">
-        <button class="btn btn-square btn-ghost">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-        </button>
+            <a href="/" class="btn btn-ghost text-xl">AnimoRank</a>
         </div>
-        <div class="flex-1">
-        <a href="/" class="btn btn-ghost text-xl">AnimoRank</a>
+        <div class="mx-5 font-semibold">|</div>
+        <div class="gap-2">
+            <div class="flex-none">
+                <a href="/" class="btn btn-ghost text-base">Home</a>
+            </div>
+            <div class="flex-none">
+                <a href="/settings" class="btn btn-ghost text-base">Settings</a>
+            </div>
+            <div class="flex-none">
+                <a href="/feedback" class="btn btn-ghost text-base">Feedback</a>
+            </div>
         </div>
-        <div class="flex-none">
-        <button class="btn btn-square btn-ghost">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
-        </button>
+        
+        <!--Dropdown start-->
+        <div class="flex gap-2 flex-none ml-auto dropdown dropdown-bottom dropdown-end">
+            
+            <!--Avatar start-->
+            <div tabindex="0" role="button" class="flex btn btn-ghost gap-5">
+                <div class="text-base">
+                    Luke Skywalker
+                </div>
+                <div class="avatar">
+                    <div class="w-10 rounded-full">
+                      <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" alt="avatar"/>
+                    </div>
+                </div>
+            </div>
+            <!--Avatar end-->
+        
+            <ul class="dropdown-content z-[1] menu p-2 mt-5 shadow bg-base-100 rounded-box w-52">
+                <li><a href="/profile">Profile</a></li>
+                <li><a href="/logout">Logout</a></li>
+              </ul>
         </div>
+        <!--Dropdown end-->
     </div>
     <!--Navbar end-->
 
