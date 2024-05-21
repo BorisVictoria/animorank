bf87bf61f2f4fe652c8f4030a1acea515aa908b2
Khryssha Claire <khryssha_damuy@dlsu.edu.ph>
2024-05-21 16:00:09 +0800

    Start login page

--- animorank-client/src/routes/login/+page.svelte
+++ animorank-client/src/routes/login/+page.svelte
@@ -1,1 +1,34 @@
-login page
+<script>
+
+</script>
+
+<div class="flex flex-row h-screen">
+    <img class="w-2/3 object-cover" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/De_La_Salle_University_Manila_DLSU_Malate_Gokongwei_Hall_Lobby.jpg/2560px-De_La_Salle_University_Manila_DLSU_Malate_Gokongwei_Hall_Lobby.jpg" alt="AnimoRank">
+    <div class="w-1/3 justify-center">
+        <div class = "flex flex-col justify-start">
+            <div class = "flex pt-10 min-w-full justify-center">
+                <div class = "font-bold text-4xl">Log in to AnimoRank</div>
+            </div>
+            <div class = "flex flex-col pt-10 min-w-full items-center">
+                <label class="form-control w-full max-w-xs">
+                    <div class="label">
+                      <span class="label-text">Username</span>
+                    </div>
+                    <input type="text" placeholder="Username" class="input input-bordered w-full max-w-xs" />
+                </label>
+                <label class="form-control w-full max-w-xs">
+                    <div class="label">
+                      <span class="label-text">Password</span>
+                    </div>
+                    <input type="text" placeholder="Password" class="input input-bordered w-full max-w-xs" />
+                </label>
+                <div class = "flex pt-2 min-w-full justify-center">
+                    <a href="/forgot">Forgot password?</a>
+                </div>
+                <div class = "flex pt-10 min-w-full justify-center">
+                    <button class="btn btn-primary text-white">Sign In</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
